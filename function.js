55994c6e9af64f4602ddde233696e24810ee43cf
sahil13031997 <sahil13031997@gmail.com>
2021-10-17 14:02:46 +0530

    with Ui

--- function.js
+++ function.js
@@ -14,17 +14,15 @@ function openInNewWindow() {
   window.open(link);
 }
 
-function onCancelClicked() {
-  document.getElementById('viewItemButton').style.display = 'none';
+function onCancelClicked()
+{	
+	document.getElementById('viewItemButton').style.display = 'none';
 }
 
-function callOnStart() {
-  gameInstance.SendMessage('GameObject', 'callingStepMaterial', 'Bluesteel.jpg');
-  gameInstance.SendMessage('GameObject', 'callingCheekMaterial', 'Bluesteel.jpg');
-  gameInstance.SendMessage('GameObject', 'callingFloorMaterial', 'Iceberg.jpg');
-  //gameInstance.SendMessage('GameObject', 'callingsettingStageMaterial', 'Bluesteel.jpg');
-}
-
-function changeStepMaterial(str) {
-  document.getElementById('staircase-model').contentWindow.gameInstance.SendMessage('GameObject', 'callingStepMaterial', `${str}.jpg`);
+function callOnStart()
+{
+	gameInstance.SendMessage('GameObject', 'callingStepMaterial', 'Bluesteel.jpg');
+	gameInstance.SendMessage('GameObject', 'callingCheekMaterial', 'Bluesteel.jpg');
+	gameInstance.SendMessage('GameObject', 'callingFloorMaterial', 'Bluesteel.jpg');
+	//gameInstance.SendMessage('GameObject', 'callingsettingStageMaterial', 'Bluesteel.jpg');
 }
